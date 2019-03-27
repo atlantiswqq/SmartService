1333f3c89d27f62f3567c45699f37e0eb6590d46
Atlantis <383750993@qq.com>
2019-03-27 23:40:07 +0800

    create private log

--- views/responsefunc.go
+++ views/responsefunc.go
@@ -1,14 +1,22 @@
 package views
 
 import (
+	"SmartService/mylogconf"
+	"encoding/json"
 	"github.com/labstack/echo"
+	"github.com/labstack/gommon/log"
 	"net/http"
 	"strings"
 )
 
 
+var Plog *privateLog.QscLog{}
+*Plog={os.Getwd(),"Smart"}
+
 // service test
 func Ping(c echo.Context) error{
+	Plog.Info("Test GET request.")
+	Plog.All("Test GET request")
 	return c.String(http.StatusOK,"service is ok.")
 }
 
@@ -21,6 +29,11 @@ func Dialog(c echo.Context) error{
 	if err:=c.Bind(s);err !=nil{
 		return err
 	}
+	jsonStr,err:=json.Marshal(s)
+	if err !=nil{
+		log.Fatal(err)
+	}
+	Plog.Debug(string(jsonStr))
 	result := new(Sentence)
 	result.Lang = strings.ToUpper(s.Lang)
 	return c.JSON(http.StatusOK,result)
